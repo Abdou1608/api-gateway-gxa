0ab60be65ad2171cea196444f36e47a9b4093fb5
Abdou <Abdourahman.souleiman@maintech-solution.com>
2025-07-05 10:01:23 +0300

    05072025 0900 commit

--- dist/services/recherche_contrat/_.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports._ = _;
-const soap_service_1 = require("../soap.service");
-async function _(body) {
-    const soapBody = {};
-    const result = await (0, soap_service_1.sendSoapRequest)(soapBody);
-    return result;
-}
