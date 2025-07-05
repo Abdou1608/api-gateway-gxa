b4ca87e8f34dd2f7d806e9d6de7eda3a838b8bd2
Abdou <Abdourahman.souleiman@maintech-solution.com>
2025-07-05 09:47:33 +0300

    05072025 0847 commit

--- dist/server.js
+++ dist/server.js
@@ -7,11 +7,12 @@ const express_1 = __importDefault(require("express"));
 const fonctionnalit__routes_1 = require("./routes/fonctionnalit\u00E9.routes");
 const Apply_Middlewares_1 = require("./middleware/Apply-Middlewares");
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
+const HOST = '0.0.0.0'; // écoute sur toutes les interfaces réseau
 // Application des middlewares
 (0, Apply_Middlewares_1.applyGlobalMiddleware)(app);
 // Enregistrement des routes
 (0, fonctionnalit__routes_1.registerRoutes)(app);
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
     console.log(`🚀 Serveur SOAP REST Gateway démarré sur http://localhost:${PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import { registerRoutes } from './routes/fonctionnalité.routes';
 import { applyGlobalMiddleware } from './middleware/Apply-Middlewares';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT:any =process.env.PORT ?? 3000;
+const HOST = '0.0.0.0'; // écoute sur toutes les interfaces réseau
 
 // Application des middlewares
 applyGlobalMiddleware(app);
@@ -12,6 +13,6 @@ applyGlobalMiddleware(app);
 // Enregistrement des routes
 registerRoutes(app);
 
-app.listen(PORT, () => {
+app.listen(PORT,HOST,() => {
   console.log(`🚀 Serveur SOAP REST Gateway démarré sur http://localhost:${PORT}`);
 });
