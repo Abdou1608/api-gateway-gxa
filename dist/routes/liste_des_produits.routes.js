15b5583ec2857cc5a0feba872ad72573815d2405
Abdou <Abdourahman.souleiman@maintech-solution.com>
2025-07-13 00:34:58 +0300

    commit du 13072025 1234

--- dist/routes/liste_des_produits.routes.js
+++ dist/routes/liste_des_produits.routes.js
@@ -2,10 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const produit_listitems_service_1 = require("../services/liste_des_produits/produit_listitems.service");
+const BasSecurityContext_1 = require("../Model/BasSoapObject/BasSecurityContext");
 const router = (0, express_1.Router)();
 router.post('/', async (req, res) => {
+    const _BasSecurityContext = new BasSecurityContext_1.BasSecurityContext();
+    _BasSecurityContext.IsAuthenticated = true;
+    _BasSecurityContext.SessionId = req.body.BasSecurityContext._SessionId;
+    const branche = req.body.branche ?? null;
+    const entites = req.body.extentions ?? null;
+    const typeecran = req.body.typeecran ?? null;
+    const disponible = req.body.disponible ?? true;
+    console.log("-----------------------------Données Reccus Route listedesproduits req.body.BasSecurityContext ==" + JSON.stringify(req.body.BasSecurityContext));
     try {
-        const result = await (0, produit_listitems_service_1.produit_listitems)(req.body);
+        const result = await (0, produit_listitems_service_1.produit_listitems)(typeecran, branche, disponible, _BasSecurityContext);
         res.json(result);
     }
     catch (error) {
--- src/routes/liste_des_produits.routes.ts
+++ src/routes/liste_des_produits.routes.ts
@@ -1,11 +1,22 @@
 import { Router } from 'express';
 import { produit_listitems } from '../services/liste_des_produits/produit_listitems.service';
+import { BasSecurityContext } from '../Model/BasSoapObject/BasSecurityContext';
 
 const router = Router();
 
 router.post('/', async (req, res) => {
+  const _BasSecurityContext= new BasSecurityContext()
+  _BasSecurityContext.IsAuthenticated=true
+  _BasSecurityContext.SessionId=req.body.BasSecurityContext._SessionId
+ const branche =req.body.branche  ?? null
+ const entites=req.body.extentions ?? null
+ const typeecran=req.body.typeecran ?? null
+const disponible=req.body.disponible ?? true
+console.log("-----------------------------Données Reccus Route listedesproduits req.body.BasSecurityContext =="+JSON.stringify( req.body.BasSecurityContext))
+ 
+
   try {
-    const result = await produit_listitems(req.body);
+    const result = await produit_listitems(typeecran,branche,disponible,_BasSecurityContext);
     res.json(result);
   } catch (error:any) {
     res.status(500).json({ error: error.message });
