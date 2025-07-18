4c7f75962469c29d42591c22621b120c3a4cbaca
Abdou <Abdourahman.souleiman@maintech-solution.com>
2025-07-18 04:58:03 +0300

    commit du 18072025 0457

--- dist/validators/api_profileValidator.js
+++ dist/validators/api_profileValidator.js
@@ -5,6 +5,6 @@ const zod_1 = require("zod");
 exports.api_profileValidator = zod_1.z.object({
     domain: zod_1.z.string().min(1, "champ domain est requis"),
     email: zod_1.z.string().optional().refine(v => v === undefined || v.length > 0, "Champ email doit être non vide si présent"),
-    username: zod_1.z.string().min(1, "champ username est requis"),
+    login: zod_1.z.string().min(1, "champ login est requis"),
     dossier: zod_1.z.string().optional().refine(v => v === undefined || v.length > 0, "champ dossier doit être non vide si présent"),
 });
