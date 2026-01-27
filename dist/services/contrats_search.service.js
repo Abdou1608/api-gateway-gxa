e19d35b36050ea0cd80f1fd9b21db017ec7ed566
Abdou <Abdourahman.souleiman@maintech-solution.com>
2026-01-27 03:52:40 +0300

    Update contrats search

--- dist/services/contrats_search.service.js
+++ dist/services/contrats_search.service.js
@@ -15,5 +15,6 @@ async function contrats_search(BasSecurityContext, reference, ctx) {
     // const soapBody = {reference,dppname,typetiers,codp,datenais}
     let result;
     result = await (0, soap_service_1.sendSoapRequest)(params, "Cont_Search", BasSecurityContext, "searchresult", undefined, ctx);
+    // const toreturn=  groupByTypename(result, { keepUnknown: true });
     return result;
 }
--- src/services/contrats_search.service.ts
+++ src/services/contrats_search.service.ts
@@ -1,6 +1,7 @@
 import { BasParams } from "../Model/BasSoapObject/BasParams";
 import {BasSecurityContext } from "../Model/BasSoapObject/BasSecurityContext";
 import { Tier } from "../Model/tier.model";
+import groupByTypename from "../utils/groupByTypename";
 import { sendSoapRequest } from "./soap.service";
 
 
@@ -28,5 +29,37 @@ export async function contrats_search(
      undefined,
      ctx
    );
-  return result;
+  // const toreturn=  groupByTypename(result, { keepUnknown: true });
+   
+  return transformSearchResultToContArray(result);
 }
+export function transformSearchResultToContArray(input: any) {
+  if (!input || typeof input !== "object") return [];
+
+  const items = Array.isArray(input.searchresult) ? input.searchresult : [];
+  const typename = typeof input.target === "string" ? input.target.toLowerCase() : null;
+
+
+  return items.map((item: { contrat: null; derpiece: null; intitule: null; police: null | undefined; }) => {
+    const out: any = {};
+
+    // Champs présents dans la source
+    out.Contrat = item?.contrat ?? null;
+    out.Derpiece = item?.derpiece ?? null;
+    out.Intitule = item?.intitule ?? null;
+
+    // police + ext_poli_police (si présent)
+    if (item?.police !== undefined && item?.police !== null) {
+      const p = String(item.police);
+      out.Polinter = p;
+      out.police = p;
+      out.Police= p;
+      out.ext_poli_police = p;
+    }
+
+    return out;
+  });
+}
+
+
+
